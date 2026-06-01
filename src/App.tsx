3ae2c9b9584d85cdc572a7aaa1b83a8d8d207320
almaleek <almaleek2324@gmail.com>
2026-06-01 14:09:13 +0100

    Switch from TanStack Router to simple Vite + React setup

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Toaster } from "@/components/ui/sonner";
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { Issues } from "@/components/Issues";
+import { HowItWorks } from "@/components/HowItWorks";
+import { ContactForm } from "@/components/ContactForm";
+import { FAQ } from "@/components/FAQ";
+import { Footer } from "@/components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Issues />
+      <HowItWorks />
+      <ContactForm />
+      <FAQ />
+      <Footer />
+      <Toaster richColors position="top-right" theme="dark" />
+    </>
+  );
+}
+
+export default App;
